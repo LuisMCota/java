b2158ed4aa6cc8331d2fd753551b92087b1cc8a1
Luis Cota <luis.f.m.c@outlook.com>
2022-06-29 23:27:46 -0500

    commit

---
+++ javascript/website/index.js
@@ -0,0 +1,19 @@
+console.log("I like pizza");
+//this is a comment
+
+/*
+hola
+hola
+*/
+//datatypes
+let firstname = "Luis";
+let age = 21;
+let student = false;
+age + age + 1;
+
+console.log("Yoy are", age, "years old.")
+console.log("Hello", firstname);
+console.log("Enrolled", student);
+document.getElementById("p1").innerHTML = "Hello" + firstname;
+document.getElementById("p2").innerHTML = "You are" + age + "years old";
+document.getElementById("p3").innerHTML = "Enrolled" + student;
